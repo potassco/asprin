ddc2a4a4c836288d5d19b25f50047f25ad6bb3a5
Javier Romero Davila <javier@Javiers-MacBook-Air.local>
2017-06-07 08:56:10 +0200

    minor

--- src/program_parser/transitive.py
+++ src/program_parser/transitive.py
@@ -21,17 +21,16 @@ class TransitiveClosure:
         self.__nodes = {}
 
     def add(self, a, b):
-        keya, keyb   = a.key, b.key
-        itema, itemb = a.item, b.item
-        prev, next   = None, None
-        nodea = self.__nodes.get(keya)
-        nodeb = self.__nodes.get(keyb)
+        # nodea
+        nodea = self.__nodes.get(a.key)
         if not nodea:
-            nodea = Node(keya, itema)
-            self.__nodes[keya] = nodea
+            nodea = Node(a.key, a.item)
+            self.__nodes[a.key] = nodea
+        # nodeb
+        nodeb = self.__nodes.get(b.key)
         if not nodeb:
-            nodeb = Node(keyb, itemb)
-            self.__nodes[keyb] = nodeb
+            nodeb = Node(b.key, b.item)
+            self.__nodes[b.key] = nodeb
         # next
         next = nodeb.next.copy()
         next.add(nodeb)
@@ -52,7 +51,6 @@ class TransitiveClosure:
         return out
 
 if __name__ == "__main__":
-    graph = [(2,1),(1,1),(1,2),(2,3),(3,4)]
     graph = [(5,2), (6,1), (2,5), (2,3), (5,1), (1,2), (2,6)]
     tc = TransitiveClosure()
     for i in graph:
