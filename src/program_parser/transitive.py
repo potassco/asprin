58263f4f78aaf0129c4d5953c98761412fa2ec6e
Javier Romero Davila <javier@Javiers-MacBook-Air.local>
2017-06-07 08:51:53 +0200

    added transitive class

---
+++ src/program_parser/transitive.py
@@ -0,0 +1,61 @@
+from collections import namedtuple
+
+Info = namedtuple('Info','key item')
+
+class Node:
+
+    def __init__(self, key, item):
+        self.key  = key
+        self.item = item
+        self.next = set()
+        self.prev = set()
+    
+    def __str__(self):
+        if self.next:
+            return '\n'.join(["({},{})".format(self.item,j.item) for j in self.next])
+        return "({})".format(self.item)
+        
+class TransitiveClosure:
+
+    def __init__(self):
+        self.__nodes = {}
+
+    def add(self, a, b):
+        keya, keyb   = a.key, b.key
+        itema, itemb = a.item, b.item
+        prev, next   = None, None
+        nodea = self.__nodes.get(keya)
+        nodeb = self.__nodes.get(keyb)
+        if not nodea:
+            nodea = Node(keya, itema)
+            self.__nodes[keya] = nodea
+        if not nodeb:
+            nodeb = Node(keyb, itemb)
+            self.__nodes[keyb] = nodeb
+        # next
+        next = nodeb.next.copy()
+        next.add(nodeb)
+        nodea.next.update(next)
+        for i in nodea.prev:
+            i.next.update(next)
+        # prev
+        prev = nodea.prev.copy()
+        prev.add(nodea)
+        nodeb.prev.update(prev)
+        for i in nodeb.next:
+            i.prev.update(prev)
+
+    def __str__(self):
+        out = ""
+        for key, item in self.__nodes.items():
+            out += str(item) + "\n"
+        return out
+
+if __name__ == "__main__":
+    graph = [(2,1),(1,1),(1,2),(2,3),(3,4)]
+    graph = [(5,2), (6,1), (2,5), (2,3), (5,1), (1,2), (2,6)]
+    tc = TransitiveClosure()
+    for i in graph:
+        tc.add(Info(i[0],i[0]),Info(i[1],i[1]))
+    print tc
+
