b7ccb495fdb75a3d157532f8cb16d9d9b0011eb8
Javier Romero <javier@cs.uni-potsdam.de>
2017-06-07 14:12:53 +0200

    progress on transitive closure

--- src/program_parser/transitive.py
+++ src/program_parser/transitive.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
 
+
 Info = namedtuple('Info','key item')
+
 
 class Node:
 
@@ -9,40 +11,91 @@ class Node:
         self.item = item
         self.next = set()
         self.prev = set()
+        self.not_next = set()
+        self.not_prev = set()
     
     def __str__(self):
+        out = []
         if self.next:
-            return '\n'.join(["({},{})".format(self.item,j.item) for j in self.next])
-        return "({})".format(self.item)
+            out += [(i.item,"+") for i in self.next]
+        if self.not_next:
+            out += [(i.item,"-") for i in self.not_next]
+        if out == []:
+            return "({})".format(self.item)
+        else:
+            out = ["({},{},{})".format(self.item,i[0],i[1]) for i in out]
+            return "\n".join(out)
         
+
 class TransitiveClosure:
 
     def __init__(self):
         self.__nodes = {}
 
-    def add(self, a, b):
-        # nodea
-        nodea = self.__nodes.get(a.key)
-        if not nodea:
-            nodea = Node(a.key, a.item)
-            self.__nodes[a.key] = nodea
-        # nodeb
-        nodeb = self.__nodes.get(b.key)
-        if not nodeb:
-            nodeb = Node(b.key, b.item)
-            self.__nodes[b.key] = nodeb
+    # add set_1 to set_2, and delete set_1 from set_3
+    def __update(self, set_1, set_2, set_3):
+        set_2.update(set_1)
+        set_3.difference_update(set_1)
+    
+    def add(self, a, b, sign):
+        
+        # node_a
+        node_a = self.__nodes.get(a.key)
+        if not node_a:
+            node_a = Node(a.key, a.item)
+            self.__nodes[a.key] = node_a
+        
+        # node_b
+        node_b = self.__nodes.get(b.key)
+        if not node_b:
+            node_b = Node(b.key, b.item)
+            self.__nodes[b.key] = node_b
+        
         # next
-        next = nodeb.next.copy()
-        next.add(nodeb)
-        nodea.next.update(next)
-        for i in nodea.prev:
-            i.next.update(next)
+        if sign:
+            next = node_b.next.copy()
+            next.add(node_b)
+            node_a.next.update(next)
+            for i in node_a.prev:
+                i.next.update(next)
+            for i in node_a.not_prev:
+                self.__update(next, i.not_next, i.next)
+        
+        # not_next
+        if sign:
+            not_next = node_b.not_next
+        else:
+            not_next = node_b.not_next.union(node_b.next)
+            not_next.add(node_b)
+        if not_next:
+            self.__update(not_next, node_a.not_next, node_a.next)
+            for i in node_a.prev:
+                self.__update(not_next, i.not_next, i.next)
+            for i in node_a.not_prev:
+                self.__update(not_next, i.not_next, i.next)
+        
         # prev
-        prev = nodea.prev.copy()
-        prev.add(nodea)
-        nodeb.prev.update(prev)
-        for i in nodeb.next:
-            i.prev.update(prev)
+        if sign:
+            prev = node_a.prev.copy()
+            prev.add(node_a)
+            node_b.prev.update(prev)
+            for i in node_b.next:
+                i.prev.update(prev)
+            for i in node_b.not_next:
+                self.__update(prev, i.not_prev, i.prev)
+        
+        # not_prev
+        if sign:
+            not_prev = node_a.not_prev
+        else:
+            not_prev = node_a.not_prev.union(node_a.prev)
+            not_prev.add(node_a)
+        if not_prev:
+            self.__update(not_prev, node_b.not_prev, node_b.prev)
+            for i in node_b.next:
+                self.__update(not_prev, i.not_prev, i.prev)
+            for i in node_a.not_next:
+                self.__update(not_prev, i.not_prev, i.prev)
 
     def __str__(self):
         out = ""
@@ -50,10 +103,11 @@ class TransitiveClosure:
             out += str(item) + "\n"
         return out
 
+
 if __name__ == "__main__":
-    graph = [(5,2), (6,1), (2,5), (2,3), (5,1), (1,2), (2,6)]
+    graph = [(1,2,True), (2,3,True), (3,4,False), (4,5,True), (5,5,True), (2,1,False), (5,1,True)]
     tc = TransitiveClosure()
     for i in graph:
-        tc.add(Info(i[0],i[0]),Info(i[1],i[1]))
+        tc.add(Info(i[0],i[0]),Info(i[1],i[1]),i[2])
     print tc
 
