a61650b544860696b34ccd777b84cf55f692b238
Javier Romero <javier@cs.uni-potsdam.de>
2018-09-13 18:04:43 +0200

    progress on meta

---
+++ asprin/src/solver/metasp/metasp.py
@@ -0,0 +1,251 @@
+# MIT License
+#
+# Copyright (c) 2017 Javier Romero
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -*- coding: utf-8 -*-
+
+#
+# IMPORTS
+#
+
+import clingo
+from . import meta_programs
+from . import scc
+
+class Observer:
+
+    def __init__(self, control, replace = False):
+        control.register_observer(self, replace)
+        self.rules = []
+        self.weight_rules = []
+        self.output_atoms = []
+        self.output_terms = []
+        self.statements = []
+        self.base = None           # (program, old constants, new constants)
+        self.specification = None  # (program, old constants, new constants)
+        self.constants_nb = None   # (program,            [],            [])
+
+    #
+    # control object observer
+    #
+
+    def rule(self, choice, head, body):
+        self.rules.append((choice, head, body))
+
+    def weight_rule(self, choice, head, lower_bound, body):
+        self.weight_rules.append((choice, head, lower_bound, body))
+
+    def output_atom(self, symbol, atom):
+        self.output_atoms.append((symbol, atom))
+
+    def output_term(self, symbol, condition):
+        self.output_terms.append((symbol, condition))
+
+    #
+    # program parser observer
+    #
+
+    def add_statement(self, statement):
+        self.statements.append(statement)
+
+    def add_base(self, program, old, new):
+        self.base = (program, old, new)
+
+    def add_specification(self, program, old, new):
+        self.specification = (program, old, new)
+
+    def add_constants_nb(self, program, old, new):
+        self.constants_nb = (program, old, new)
+
+
+class Metasp:
+
+    # TODO: Implement option where we take care about repeated heads and bodies
+    def reify(self, observer, prefix=""):
+
+        # start
+        out = ""
+        literal_tuple, wliteral_tuple, atom_tuple = 1, 1, 1
+        p = prefix
+
+        # fact 0
+        # out += "% fact 0\n"
+        out += "{}rule(disjunction(0),normal(0)). {}atom_tuple(0,0). {}literal_tuple(0).\n\n".format(p, p, p)
+
+        # start graph
+        graph = scc.Graph()
+
+        # normal rules
+        for choice, head, body in observer.rules:
+
+            # out += "% normal rule\n"
+            # body
+            out += "{}literal_tuple({}).".format(p, literal_tuple) + "\n"
+            out += " ".join(["{}literal_tuple({},{}).".format(p, literal_tuple, l) for l in body]) + "\n"
+            # head
+            head_type = "choice" if choice else "disjunction"
+            out += "{}rule({}({}),normal({})).".format(p, head_type, atom_tuple, literal_tuple) + "\n"
+            out += " ".join(["{}atom_tuple({},{}).".format(p, atom_tuple, l) for l in head]) + "\n\n"
+            # update counters
+            literal_tuple += 1
+            atom_tuple += 1
+            # update graph (this can be interwined with the rules above)
+            for l in body:
+                if l >= 0:
+                    for atom in head:
+                        graph.add_edge(atom, l)
+
+
+        # weight rules
+        for choice, head, lower_bound, body in observer.weight_rules:
+            # out += "% weighted rule\n"
+            # body
+            out += " ".join(["{}weighted_literal_tuple({},{},{}).".format(p, wliteral_tuple, l, w) for l, w in body]) + "\n"
+            # head
+            head_type = "choice" if choice else "disjunction"
+            out += "{}rule({}({}),sum({},{})).".format(p, head_type, atom_tuple, wliteral_tuple, lower_bound) + "\n"
+            out += " ".join(["{}atom_tuple({},{}).".format(p, atom_tuple, l) for l in head]) + "\n\n"
+            # update counters
+            wliteral_tuple += 1
+            atom_tuple += 1
+            # update graph (this can be interwined with the rules above)
+            for l, w in body:
+                if l >= 0:
+                    for atom in head:
+                        graph.add_edge(atom, l)
+
+        # sccs
+        # out += "% sccs\n"
+        out += graph.reify_sccs(p) + "\n"
+
+        # output atoms
+        # out += "% output atoms\n"
+        for symbol, atom in observer.output_atoms:
+            out += "{}output({},{}).\n".format(p, symbol, atom)
+
+        # output terms
+        # out += "% output term\n"
+        for symbol, condition in observer.output_terms:
+            out += "{}output_term({},{}).\n".format(p, symbol, condition[0])
+
+        return out + meta_programs.metaD_program.replace("##", p)
+
+
+
+def run(base, metaD=False):
+
+    # observe rules
+    ctl = clingo.Control(["0"])
+    #observer = Observer(ctl, False)
+    observer = Observer(ctl, True)
+    ctl.add("base", [], base)
+    ctl.ground([("base", [])])
+    #models0 = []
+    #with ctl.solve(yield_=True) as handle:
+    #    for m in handle:
+    #        models0.append(" ".join(sorted([str(x) for x in m.symbols(shown=True)])))
+    #    # print(handle.get())
+    #models0 = sorted(models0)
+
+    # use reified version
+    base = observer.reify()
+    print(base)
+    return
+    if metaD:
+        base += meta_programs.metaD_program
+    else:
+        base += meta_programs.meta_program
+    ctl = clingo.Control(["0"])
+    ctl.add("base", [], base)
+    ctl.ground([("base", [])])
+    models1 = []
+    with ctl.solve(yield_=True) as handle:
+        for m in handle:
+            models1.append(" ".join(sorted([str(x) for x in m.symbols(shown=True)])))
+        # print(handle.get())
+    models1 = sorted(models1)
+
+    # check and print
+    print("ERROR" if models0 != models1 else "OK")
+    if models0 != models1:
+        print(models0)
+        print(models1)
+    if len(models0):
+        models0[0] += "_x "
+        print("OK" if models0 != models1 else "ERROR")
+
+
+#
+# programs
+#
+
+basic = """
+{a}. b.
+"""
+
+aggregates = """
+{a(X) : dom(X)}.
+b(X) :- X = { a(Y) }.
+dom(1..2).
+#show b/1.
+"""
+
+# pigeon hole
+pigeon = """
+#const n=6.
+pigeon(1..n+1). box(1..n+1).
+1 { in(X,Y) : box(Y) } 1 :- pigeon(X).
+:- 2 { in(X,Y) : pigeon(X) },  box(Y).
+a(X,Y,Z) :- in(X,Y), in(Y,Z).
+"""
+
+loop = """
+a :- b.
+b :- a.
+b :- c.
+a :- c.
+{ c }.
+"""
+
+many_loops = """
+dom(1..320).
+{ edge(X,Y) : dom(X), dom(Y) }.
+tr(X,Y) :- edge(X,Y).
+tr(X,Y) :- tr(X,Z), tr(Z,Y).
+:- tr(X,X).
+"""
+programs = [basic, aggregates, pigeon, loop, many_loops]
+#programs = [""" a ; b :- 1 #sum {1:b; 1:c}. {c}."""]
+programs = [many_loops]
+if __name__ == "__main__":
+    # meta
+    for program in programs:
+        print("##### META  #######")
+        print(program)
+        run(program)
+        print("#####################")
+    programs = []
+    # metaD
+    for program in programs:
+        print("##### METAD #######")
+        print(program)
+        run(program, True)
+        print("#####################")
+
---
+++ asprin/src/solver/metasp/metasp_programs.py
@@ -0,0 +1,152 @@
+meta_program = """
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file is part of clingo.                                            %
+% Copyright (C) 2015  Martin Gebser                                       %
+% Copyright (C) 2015  Roland Kaminski                                     %
+% Copyright (C) 2015  Torsten Schaub                                      %
+%                                                                         %
+% This program is free software: you can redistribute it and/or modify    %
+% it under the terms of the GNU General Public License as published by    %
+% the Free Software Foundation, either version 3 of the License, or       %
+% (at your option) any later version.                                     %
+%                                                                         %
+% This program is distributed in the hope that it will be useful,         %
+% but WITHOUT ANY WARRANTY; without even the implied warranty of          %
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           %
+% GNU General Public License for more details.                            %
+%                                                                         %
+% You should have received a copy of the GNU General Public License       %
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+##conjunction(B) :- ##literal_tuple(B),
+        ##hold(L) : ##literal_tuple(B, L), L > 0;
+    not ##hold(L) : ##literal_tuple(B,-L), L > 0.
+
+##body(normal(B)) :- ##rule(_,normal(B)), ##conjunction(B).
+##body(sum(B,G))  :- ##rule(_,sum(B,G)),
+    #sum { W,L :     ##hold(L), ##weighted_literal_tuple(B, L,W), L > 0 ;
+           W,L : not ##hold(L), ##weighted_literal_tuple(B,-L,W), L > 0 } >= G.
+
+  ##hold(A) : ##atom_tuple(H,A)   :- ##rule(disjunction(H),B), ##body(B).
+{ ##hold(A) : ##atom_tuple(H,A) } :- ##rule(     choice(H),B), ##body(B).
+
+% commented by Javier
+%*
+##optimize(J,W,Q) :- ##output(_optimize(J,W,Q),B), ##conjunction(B).
+ :- ##output(_query,B), not ##conjunction(B).
+
+##hide(_criteria(J,W,Q)) :- ##output(_criteria(J,W,Q),_).
+##hide(_query)           :- ##output(_query,_).
+##hide(_optimize(J,W,Q)) :- ##output(_optimize(J,W,Q),_).
+#show.
+#show T : ##output(T,B), ##conjunction(B), not ##hide(T).
+*%
+
+% added by Javier
+#show.
+#show T : ##output(T,L), ##hold(L).
+
+
+"""
+
+metaD_program = """
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file is part of clingo.                                            %
+%                                                                         %
+% Authors: Martin Gebser, Roland Kaminski, Torsten Schaub                 %
+%                                                                         %
+% This program is free software: you can redistribute it and/or modify    %
+% it under the terms of the GNU General Public License as published by    %
+% the Free Software Foundation, either version 3 of the License, or       %
+% (at your option) any later version.                                     %
+%                                                                         %
+% This program is distributed in the hope that it will be useful,         %
+% but WITHOUT ANY WARRANTY; without even the implied warranty of          %
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           %
+% GNU General Public License for more details.                            %
+%                                                                         %
+% You should have received a copy of the GNU General Public License       %
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% NOTE: assumes that a rule has no more than one head
+
+##sum(B,G,T) :- ##rule(_,sum(B,G)), T = #sum { W,L : ##weighted_literal_tuple(B,L,W) }.
+
+% extract supports of atoms and facts
+
+##supp(A,B) :- ##rule(     choice(H),B), ##atom_tuple(H,A).
+##supp(A,B) :- ##rule(disjunction(H),B), ##atom_tuple(H,A).
+
+##supp(A) :- ##supp(A,_).
+
+##atom(|L|) :- ##weighted_literal_tuple(_,L,_).
+##atom(|L|) :- ##literal_tuple(_,L).
+##atom( A ) :- ##atom_tuple(_,A).
+
+##fact(A) :- ##rule(disjunction(H),normal(B)), ##atom_tuple(H,A), not ##literal_tuple(B,_).
+
+% generate interpretation
+
+##true(atom(A))                :- ##fact(A).
+##true(atom(A)); ##fail(atom(A)) :- ##supp(A), not ##fact(A).
+                 ##fail(atom(A)) :- ##atom(A), not ##supp(A).
+
+##true(normal(B)) :- ##literal_tuple(B),
+    ##true(atom(L)) : ##literal_tuple(B, L), L > 0;
+    ##fail(atom(L)) : ##literal_tuple(B,-L), L > 0.
+##fail(normal(B)) :- ##literal_tuple(B, L), ##fail(atom(L)), L > 0.
+##fail(normal(B)) :- ##literal_tuple(B,-L), ##true(atom(L)), L > 0.
+
+##true(sum(B,G)) :- ##sum(B,G,T),
+    #sum { W,L : ##true(atom(L)), ##weighted_literal_tuple(B, L,W), L > 0 ;
+           W,L : ##fail(atom(L)), ##weighted_literal_tuple(B,-L,W), L > 0 } >= G.
+##fail(sum(B,G)) :- ##sum(B,G,T),
+    #sum { W,L : ##fail(atom(L)), ##weighted_literal_tuple(B, L,W), L > 0 ;
+           W,L : ##true(atom(L)), ##weighted_literal_tuple(B,-L,W), L > 0 } >= T-G+1.
+
+% verify supported model properties
+
+##bot :- ##rule(disjunction(H),B), ##true(B), ##fail(atom(A)) : ##atom_tuple(H,A).
+##bot :- ##true(atom(A)), ##fail(B) : ##supp(A,B).
+
+% verify acyclic derivability
+
+##internal(C,normal(B)) :- ##scc(C,A), ##supp(A,normal(B)), ##scc(C,A'), ##literal_tuple(B,A').
+##internal(C,sum(B,G))  :- ##scc(C,A), ##supp(A,sum(B,G)),  ##scc(C,A'), ##weighted_literal_tuple(B,A',W).
+
+##external(C,normal(B)) :- ##scc(C,A), ##supp(A,normal(B)), not ##internal(C,normal(B)).
+##external(C,sum(B,G))  :- ##scc(C,A), ##supp(A,sum(B,G)),  not ##internal(C,sum(B,G)).
+
+##steps(C,Z-1) :- ##scc(C,_), Z = { ##scc(C,A) : not ##fact(A) }.
+
+##wait(C,atom(A),0)   :- ##scc(C,A), ##fail(B) : ##external(C,B).
+##wait(C,normal(B),I) :- ##internal(C,normal(B)), ##literal_tuple(B,A), ##wait(C,atom(A),I), ##steps(C,Z), I < Z.
+##wait(C,sum(B,G),I)  :- ##internal(C,sum(B,G)), ##steps(C,Z), I = 0..Z-1, ##sum(B,G,T),
+    #sum { W,L :   ##fail(atom(L)),   ##weighted_literal_tuple(B, L,W), L > 0, not ##scc(C,L) ;
+           W,L : ##wait(C,atom(L),I), ##weighted_literal_tuple(B, L,W), L > 0,     ##scc(C,L) ;
+           W,L :   ##true(atom(L)),   ##weighted_literal_tuple(B,-L,W), L > 0               } >= T-G+1.
+##wait(C,atom(A),I)   :- ##wait(C,atom(A),0), ##steps(C,Z), I = 1..Z, ##wait(C,B,I-1) : ##supp(A,B), ##internal(C,B).
+
+##bot :- ##scc(C,A), ##true(atom(A)), ##wait(C,atom(A),Z), ##steps(C,Z).
+
+% saturate interpretations that are not answer sets
+
+##true(atom(A)) :- ##supp(A), not ##fact(A), ##bot.
+##fail(atom(A)) :- ##supp(A), not ##fact(A), ##bot.
+
+%
+% added by Javier
+%
+
+%#show.
+%#show T : ##output(T,L), ##true(atom(L)).
+#defined ##literal_tuple/1.
+#defined ##literal_tuple/2.
+#defined ##rule/2.
+#defined ##atom_tuple/2.
+#defined ##weighted_literal_tuple/3.
+#defined ##scc/2.
+"""
